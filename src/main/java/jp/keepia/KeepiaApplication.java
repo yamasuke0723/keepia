ceddfbba9612d24c04468d142b0e769eb08bfe13
bonzu2235 <bonzu2235.gmail.com>
2026-09-15 00:38:29 +0900

    プロジェクトファイルを作成し、格納します。

---
+++ src/main/java/jp/keepia/KeepiaApplication.java
@@ -0,0 +1,13 @@
+package jp.keepia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/keepia/KeepiaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.keepia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
